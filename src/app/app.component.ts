f8a3e96c8bd6664867ece3ee797e83b122466a5b
Bynio <karol.nowak0490@protonmail.com>
2024-12-25 01:20:15 +0100

    feat: create card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
+import { CardComponent } from "./card/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ NavigationComponent],
+  imports: [NavigationComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

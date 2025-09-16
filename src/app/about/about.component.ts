0f6ddef743239db176dc00d4457cfe7eb131008a
byniov1 <karol.nowak0490@protonmail.com>
2025-09-16 15:20:28 +0200

    feat: create first section component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonsModule } from '../common/common.module';
+import { FirstSectionComponent } from './first-section/first-section.component';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonsModule],
+  imports: [CommonsModule, FirstSectionComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   standalone: true,
---
+++ src/app/about/first-section/first-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-section',
+  imports: [],
+  templateUrl: './first-section.component.html',
+  styleUrl: './first-section.component.scss'
+})
+export class FirstSectionComponent {
+
+}

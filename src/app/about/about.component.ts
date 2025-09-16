a0b61958ea5b9a1c4d87419957264405c8e44ab4
byniov1 <karol.nowak0490@protonmail.com>
2025-09-16 16:00:29 +0200

    feat: create init experience section, fixes in first section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonsModule } from '../common/common.module';
 import { FirstSectionComponent } from './first-section/first-section.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonsModule, FirstSectionComponent],
+  imports: [CommonsModule, FirstSectionComponent, ExperienceComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   standalone: true,

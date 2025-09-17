469ac7edd3777cf531ac2ddb6c6d6ffac5054e7a
byniov1 <karol.nowak0490@protonmail.com>
2025-09-17 14:03:53 +0200

    feat: create skills section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { CommonsModule } from '../common/common.module';
 import { FirstSectionComponent } from './first-section/first-section.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonsModule, FirstSectionComponent, ExperienceComponent],
+  imports: [
+    CommonsModule,
+    FirstSectionComponent,
+    ExperienceComponent,
+    SkillsComponent,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   standalone: true,
---
+++ src/app/about/skills/skills.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../../common/card/card.component';
+
+@Component({
+  selector: 'app-skills',
+  imports: [CardComponent],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.scss',
+})
+export class SkillsComponent {}
